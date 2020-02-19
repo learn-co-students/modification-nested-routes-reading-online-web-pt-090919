d99c2b542ba766ddd9abb9a05d8fde424ccffebd
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-02-18 23:19:18 -0500

    Done.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,12 @@
 module PostsHelper
-  
+  # we can only have one :author_id
+  def author_id_field(post)
+    if post.author.nil?
+      select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+    else
+      # Add hidden field form_for :author_id when we browse to
+      #  /authors/1/posts/new
+      hidden_field_tag "post[author_id]", post.author_id
+    end
+  end
 end
